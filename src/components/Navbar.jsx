4fe84216d42c2a931dfab5bcdc776c4f09f76766
Supriya Dhekane <dhekanesupriya7@gmail.com>
2025-08-12 17:02:59 +0530

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,30 @@
-import { Outlet,Link } from "react-router-dom"
-import InPageNavigation from "./inPageNavigation"
-
+import { Outlet, Link } from "react-router-dom";
+import InPageNavigation from "./InPageNavigation";
 
 function Navbar() {
   return (
-  <div className="min-h-screen bg-white text-black">
-    
-     <InPageNavigation teams={[{title:"Match",sppath:"/"},{title:"News",sppath:"/news"},{title:"Table",sppath:"/pointsTable"}]} >
-     <Link to={"/"}><p>Match</p></Link>
-     <Link to={"/news"}><p>News</p></Link>
-      <Link to={"/pointsTable"}><p>Table</p></Link>
-   
-     
-     </InPageNavigation>
- 
-    <Outlet></Outlet>
-  </div>
+    <div className="min-h-screen bg-white text-black">
+      <InPageNavigation
+        teams={[
+          { title: "Match", sppath: "/" },
+          { title: "News", sppath: "/news" },
+          { title: "Table", sppath: "/pointsTable" },
+        ]}
+      >
+        <Link to={"/"}>
+          <p>Match</p>
+        </Link>
+        <Link to={"/news"}>
+          <p>News</p>
+        </Link>
+        <Link to={"/pointsTable"}>
+          <p>Table</p>
+        </Link>
+      </InPageNavigation>
 
-
-  )
+      <Outlet></Outlet>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
